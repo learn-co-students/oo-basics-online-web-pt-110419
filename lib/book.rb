66d9b27cdc5cdd8c546a347bf7d0db5e6b4ee795
Anthony Mai <anthony.mai530@gmail.com>
2019-11-24 01:46:34 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,9 @@
 class Book
+  attr_reader :title
+  attr_accessor(:author, :page_count, :genre)
   def initialize(title)
     @title = title
   end
-  def title
-    @title
-  end
-  attr_accessor(:author, :page_count, :genre)
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
 end
