1a52d6f8894f7eefbfbfeacdfb700b049d110dc4
Mintra Phophaijit <bluembc@gmail.com>
2019-12-26 04:06:44 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,35 +1,13 @@
 class Shoe
-  
- def brand("nike")
-   @nike = nike
- end 
- 
- shoe.new("addidas")
- 
- def shoe.color("red")
-   @red = red 
- end 
- 
- def shoe.size("9.5")
-  @9.5 = 9.5
-end 
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
-def shoe.material("suede")
-  @suede = suede 
-end 
+  def initialize(brand)
+    @brand = brand
+  end
 
-def shoe.condition("tattered")
-  @tattered = tattered
-end 
-
-def shoe.cobble
-  puts "Your shoe is as good as new!"
-end 
- 
-def shoe.condition("old")
-  @old = old
-end 
-
-  
-  
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 end
